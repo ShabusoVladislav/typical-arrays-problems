c0b3c7a12129b604159ad8ccf556ba1e83443015
Vladislav Shabusov <vladshabusov66@gmail.com>
2021-08-21 01:26:05 +0300

    feat: complete typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+  
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+  
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {  
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+
+  let average = 0;
+  for(let i = 0; i < array.length; i++) {
+    average += array[i];
+  }
+  return average / array.length;
 }
